3f81ced131ee985065ff4f963ab320af0e439f6b
Michael H <MichaelHahmigery@gmail.com>
2020-07-03 19:15:14 +0300

    Create validation schema for registering.

---
+++ src/schemas/register.ts
@@ -0,0 +1,20 @@
+import joi from "@hapi/joi";
+
+export const registerSchema = joi.object({
+  username: joi.string().alphanum().min(4).max(20).required(),
+
+  password: joi
+    .string()
+    .pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,30}$/)
+    .required(),
+
+  name: joi
+    .string()
+    .pattern(/^[A-Za-z]{2,20}$/)
+    .required(),
+
+  lastname: joi
+    .string()
+    .pattern(/^[A-Za-z]{2,20}$/)
+    .required(),
+});
