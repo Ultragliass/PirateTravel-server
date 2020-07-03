eddbf3d473918a9da44921b1efe7e6f00075f1bb
Michael H <MichaelHahmigery@gmail.com>
2020-07-03 18:48:28 +0300

    Create JWT path whitelist.

--- src/server.ts
+++ src/server.ts
@@ -3,16 +3,22 @@ import http from "http";
 import socketIo from "socket.io";
 import cors from "cors";
 import expressJwt from "express-jwt";
+import { JWT_SECRET } from "./secret";
 
-const PORT = process.env.PORT || 3001;
+const PORT: string | number = process.env.PORT || 3001;
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-const { JWT_SECRET = "secret" } = process.env;
-
 app.use(express.json());
+
 app.use(cors());
+
+app.use(
+  expressJwt({ secret: JWT_SECRET }).unless({ path: ["/register", "/login"] })
+);
+
+
 
 app.get("/", (req, res) => {
   res.send("Hi there!");
