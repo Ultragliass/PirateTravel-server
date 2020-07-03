b28ab49d1a2e58a36f497f4ba915a29c1cebaee0
Michael H <MichaelHahmigery@gmail.com>
2020-07-03 18:44:01 +0300

    Add socket.io dependency and create socket.io supported server.

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,21 @@
-import express from 'express';
-import cors from 'cors';
-import expressJwt from 'express-jwt';
+import express from "express";
+import http from "http";
+import socketIo from "socket.io";
+import cors from "cors";
+import expressJwt from "express-jwt";
 
-const PORT = 3001;
-
-const {JWT_SECRET = 'secret'} = process.env;
-
+const PORT = process.env.PORT || 3001;
 const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const { JWT_SECRET = "secret" } = process.env;
 
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Hi there!');
-})
+app.get("/", (req, res) => {
+  res.send("Hi there!");
+});
 
-app.listen(PORT, () => console.log(`Server is up at ${PORT}`));
+server.listen(PORT, () => console.log(`Server is up at ${PORT}`));
