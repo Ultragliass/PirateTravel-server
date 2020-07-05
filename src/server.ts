1ca9ae65a3f992c432c0b0d38f654f9a18795933
Michael H <MichaelHahmigery@gmail.com>
2020-07-05 19:54:25 +0300

    Create socket.io base.

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,13 @@ import { register } from "./routers/register";
 import { login } from "./routers/login";
 import { JWT_SECRET } from "./secret";
 import { vacations } from "./routers/vacations";
+import { IVacation } from "./models/vacation";
 import express, { Request, Response, NextFunction } from "express";
 import http from "http";
-import socketIo from "socket.io";
+import socketIo, { Socket } from "socket.io";
 import cors from "cors";
 import expressJwt from "express-jwt";
+import socketioJwt from "socketio-jwt";
 
 const PORT: string | number = process.env.PORT || 3001;
 const app = express();
@@ -30,5 +32,28 @@ app.use("/vacations", vacations);
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
   res.status(401).send("Unauthorized.");
 });
+
+io.sockets
+  .on(
+    "connection",
+    socketioJwt.authorize({
+      secret: JWT_SECRET,
+    })
+  )
+  .on("authenticated", (socket: any) => {
+    const { userType } = socket.decoded_token;
+    console.log(`${userType} connected.`);
+
+    socket.on("UPDATE_VACATION", () => {
+      if (userType !== "admin") {
+        return socket.emit(
+          "UNAUTHORIZED",
+          "You do not have permission to perform this action."
+        );
+      }
+
+      socket.broadcast.emit("UPDATE_VACATIONS", "Vacation updated.");
+    });
+  });
 
 server.listen(PORT, () => console.log(`Server is up at ${PORT}`));
