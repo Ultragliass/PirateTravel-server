ec010d71b6bb5e90a546589a9fe14e757361a162
Michael H <MichaelHahmigery@gmail.com>
2020-07-15 13:52:54 +0300

    Add validation to match client.

--- src/middleware/validateDates.ts
+++ src/middleware/validateDates.ts
@@ -7,14 +7,14 @@ export const validateDates = () => {
     if (new Date(startDate) < new Date() || new Date(endDate) < new Date()) {
       return res.status(401).send({
         success: false,
-        msg: "Dates cannot be earlier than the current date.",
+        msg: "Dates cannot be earlier than the current date",
       });
     }
 
     if (new Date(startDate) >= new Date(endDate)) {
       return res.status(401).send({
         success: false,
-        msg: "Starting date must be earlier than end date.",
+        msg: "Starting date must be earlier than end date",
       });
     }
 
--- src/schemas/vacation.ts
+++ src/schemas/vacation.ts
@@ -3,10 +3,10 @@ import joi from "@hapi/joi";
 export const vacationSchema = joi.object({
   description: joi.string().empty().min(30).max(200).required(),
   destination: joi.string().empty().max(30).required(),
-  image: joi.string().empty().max(100).required(),
+  image: joi.string().empty().max(200).pattern(/(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|png|JPG|jpeg|JPEG)/).required(),
   startDate: joi.date().required(),
   endDate: joi.date().required(),
-  price: joi.number().required(),
+  price: joi.number().positive().required(),
   isFollowing: 0,
   followers: joi.number(),
 });
