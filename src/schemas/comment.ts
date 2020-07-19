a4b8922662deb726e51491af6ef620734544fe32
Michael H <MichaelHahmigery@gmail.com>
2020-07-19 17:39:37 +0300

    Fix comment validation.

--- src/schemas/comment.ts
+++ src/schemas/comment.ts
@@ -1,5 +1,5 @@
 import joi from "@hapi/joi";
 
 export const commentSchema = joi.object({
-  comment: joi.string().alphanum().empty().max(100).required(),
+  comment: joi.string().empty().max(100).required(),
 });
